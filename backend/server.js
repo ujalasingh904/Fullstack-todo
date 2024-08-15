9780731cfecc538250bd6b4f73fe9cd30df02b0f
ujala <singhujala2004@gmail.com>
2024-08-15 14:01:09 +0530

    "Update CORS origin from localhost:5173 to https://fullstack-todo-frontend-lovat.vercel.app"

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.port || 5000
 app.use(express.json())
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://fullstack-todo-frontend-lovat.vercel.app",
         credentials: true
     }
 ))
